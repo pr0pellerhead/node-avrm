179f70905422d56bc0dce1f759202a8f332ba821
Bojan Gavrovski <bojan.gavrovski@gmail.com>
2020-02-05 14:27:20 +0100

    commented the fetch for todos

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fetch = require('node-fetch');
 let f = c1.c2f(14);
 console.log(f);
 
-fetch('https://jsonplaceholder.typicode.com/todos')
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(err => console.log(err));
+// fetch('https://jsonplaceholder.typicode.com/todos')
+// .then(res => res.json())
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
